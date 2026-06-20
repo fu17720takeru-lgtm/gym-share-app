e64693d6f8f13122b8441f071d5e73dbb4d88a32
今村拓哉 <tak@imamuratakuyanoMac-mini.local>
2026-06-20 11:03:39 +0900

    FastAPI試し

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 #venvを起動するには Mac:　source venv/bin/activate  Win: venv/Scripts/activate
 from argon2 import PasswordHasher
+from fastapi import FastAPI
+
+'''
 # PasswordHasherのインスタンスを作成（デフォルト設定を使用）
 ph = PasswordHasher()
 # ハッシュ化したいパスワード
@@ -11,3 +14,10 @@ print(f"元のパスワード: {password}")
 print(f"ハッシュ化されたパスワード: {hashed_password}")
 # 生成されたハッシュ値の例 (実行ごとにソルトが異なるため、結果は変わります):
 # $argon2id$v=19$m=65536,t=3,p=4$MIIRqgvgQbgj220jfp0MPA$YfwJSVjtjSU0zzV/P3S9nnQ/USre2wvJMjfCIjrTQbg
+'''
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"messsage": "Hello World"}
